95e6384627b4cbd6239801075064bee74b46a56a
jayesh <jbairagi@grepruby.com>
2017-10-17 19:42:06 +0530

    original repo

--- src/components/VideosPage.js
+++ src/components/VideosPage.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-// First, we extract videos, onHandleSelectVideo, and selectedVideo 
-// from props using destructuring assignment and then render.
 const VideosPage = ({ videos, onHandleSelectVideo, selectedVideo }) => (
   <div className="col-md-6">
-    <h2> Videos </h2>
-
-    {console.log(selectedVideo.id)}
+    <h2>Videos</h2>
     <div className="select-video">
-      <div key={selectedVideo.id}>
+      <div id={selectedVideo.id}>
         <h6 className="title">{selectedVideo.description}</h6>
         <video controls src={selectedVideo.mediaUrl} alt={selectedVideo.title} />
       </div>
     </div>
     <div className="video-thumbnail">
-      {videos.map((video, i) => (
-        <div key={i} onClick={onHandleSelectVideo.bind(this, video)}>
+      {videos.map(video => (
+        <div key={video.id} onClick={onHandleSelectVideo.bind(this, video)}>
           <video controls src={video.mediaUrl} alt={video.description} />
         </div>
       ))}
@@ -23,6 +19,10 @@ const VideosPage = ({ videos, onHandleSelectVideo, selectedVideo }) => (
   </div>
 );
 
-
+VideosPage.propTypes = {
+  videos: PropTypes.array,
+  selectedVideo: PropTypes.object,
+  onHandleSelectVideo: PropTypes.func.isRequired
+};
 
 export default VideosPage;
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,6 @@
 import { fork } from 'redux-saga/effects';
-import watchSearchMedia from './watcher';
+import watchSearchMedia from './watchers';
 
-// Here, we register our watcher saga(s) and export as a single generator 
-// function (startForeman) as our root Saga.
 export default function* startForman() {
   yield fork(watchSearchMedia);
 }
